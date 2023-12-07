3a22d79eac5aa74cdc44ebaa864b750dc167d24f
Linsted <ivan.malenko18@gmail.com>
2023-12-07 23:41:34 +0200

    feat: add Navigation component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home/Home";
 import Navigation from "./components/Navigation";
+import Shop from "./pages/Shop/Shop";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
   );
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,9 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+
+import styles from "./Navigation.module.scss";
+
+import { ReactComponent as Logo } from "../../assets/086 crown.svg";
 
 export default function Navigation() {
   return (
     <>
-      <div>Navigation</div>
+      <header className={styles.navigation}>
+        <Link to="/" className={styles.logoContainer}>
+          <Logo />
+        </Link>
+
+        <nav className={styles.navLinksContainer}>
+          <Link className={styles.navLink} to="/shop">
+            SHOP
+          </Link>
+        </nav>
+      </header>
       <Outlet />
     </>
   );
